36de84d4d90340ae10bda7102f6dab49f26d1a15
lapshaffer <lapshaffer@gmail.com>
2021-06-23 15:27:54 -0400

    Updated ReadMe file to include assignment criteria, added pseudocode to all files

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// TO-DO:
+// 1. eventListeners for buttons
+//      timer starts when start button is clicked
+// 2. Data attributes set and maleable for different questions/switching between questions
+// 3. localStorage for high scores
+//      a place for the user to put their initials and have them stored alongside their score
+// 4. if statements for correct and incorrect answers
+//      and displaying if that answer was right or wrong on the page
+//      Time subtracted from timer when answers are incorrect
+// 5. Score is calculated from number of correct answers
